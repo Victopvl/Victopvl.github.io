74e8a3edf18d5fba4fe5094ca6ca422aabfef222
Victopvl <victoriapaz.sa@gmail.com>
2026-05-21 15:20:58 -0400

    style: implementar paleta de colores corporativa y configurar tema personalizado datavolve

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,13 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [require("@tailwindcss/typography"),require("daisyui")],
-	daisyui: {
-		themes: true, // true: all themes | false: only light + dark | array: specific themes like this ["light", "dark", "cupcake"]
-		darkTheme: "dark", // name of one of the included themes for dark mode
-		logs: false, // Shows info about daisyUI version and used config in the console when building your CSS
-	  }
+    content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+    theme: {
+        extend: {},
+    },
+    plugins: [require("@tailwindcss/typography"), require("daisyui")],
+    daisyui: {
+        themes: [
+          {
+            datavolve: {
+              "primary": "#6C8297",       // Azul Acero (Botones principales y acentos)
+              "secondary": "#A3B4C5",     // Azul Neblina (Etiquetas, badges y estados)
+              "accent": "#6C8297",        // Enfoque secundario
+              "neutral": "#CBD2D9",       // Gris Claro Neutro (Bordes finos y separadores)
+              "base-100": "#FAFAFA",      // Blanco Suave (El fondo general de la pantalla)
+              "base-200": "#FFFFFF",      // Blanco Puro (Fondo del menú lateral y tarjetas para contraste)
+              "base-content": "#3A4B5C",  // Azul Oscuro Pizarra (El color de todos los textos y títulos)
+            },
+          },
+        ],
+        darkTheme: "datavolve", // Mantenemos tu paleta limpia como base predeterminada
+        logs: false,
+    }
 }
