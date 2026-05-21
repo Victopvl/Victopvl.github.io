024a985355d5cd42147ab652b30b9e1e5031bfef
Victopvl <victoriapaz.sa@gmail.com>
2026-05-21 15:43:39 -0400

    update site rss.xml.js

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -2,12 +2,13 @@ import rss from "@astrojs/rss";
 import { SITE_TITLE, SITE_DESCRIPTION } from "../config";
 import { getCollection } from "astro:content";
 
-export async function get(context) {
+// Cambiamos 'get' por 'GET' para cumplir con la API de rutas de Astro
+export async function GET(context) {
   const blog = await getCollection("blog");
   return rss({
     title: SITE_TITLE,
     description: SITE_DESCRIPTION,
-    site: import.meta.env.SITE,
+    site: context.site, // Usamos el contexto para mayor seguridad
     items: blog.map((post) => ({
       title: post.data.title,
       pubDate: post.data.pubDate,
