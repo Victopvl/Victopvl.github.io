713afef67d51dbdbee1d6bdb4e52f69c7e959839
Victopvl <victoriapaz.sa@gmail.com>
2026-05-21 15:13:07 -0400

    fix: personalizar metadatos globales y descripcion seo para produccion

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Victoria Vallejos | Portfolio Profesional';
-export const SITE_DESCRIPTION = 'Astrofy is a free and open-source template for your Personal Portfolio Website built with Astro and TailwindCSS. Create in minutes a website with Blog, CV, Project Section, Store and RSS Feed.';
-export const GENERATE_SLUG_FROM_TITLE = true
-export const TRANSITION_API = true
+export const SITE_TITLE = 'Victoria Vallejos | Consultoría TI & Portafolio';
+export const SITE_DESCRIPTION = 'Portafolio profesional de Victoria Vallejos. Estudiante de Ingeniería Civil Informática, Managing Consultant en Datavolve y especialista en transformación digital, gestión estratégica y soluciones tecnológicas.';
+export const GENERATE_SLUG_FROM_TITLE = true;
+export const TRANSITION_API = true;
